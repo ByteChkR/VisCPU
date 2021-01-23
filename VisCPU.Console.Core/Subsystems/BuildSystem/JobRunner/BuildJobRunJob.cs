690c5e9360b357c08fd967158c5c3333a8397a25
Byt3ChkR <ti.akermann@gmail.com>
2021-01-23 16:58:48 +0100

    Fix Run Job

--- VisCPU.Console.Core/Subsystems/BuildSystem/JobRunner/BuildJobRunJob.cs
+++ VisCPU.Console.Core/Subsystems/BuildSystem/JobRunner/BuildJobRunJob.cs
@@ -1,5 +1,6 @@
 ﻿using VisCPU.ProjectSystem.BuildSystem;
 using VisCPU.ProjectSystem.Data;
+using System.IO;
 
 namespace VisCPU.Console.Core.Subsystems.BuildSystem.JobRunner
 {
@@ -16,7 +17,10 @@ namespace VisCPU.Console.Core.Subsystems.BuildSystem.JobRunner
             ProjectBuildTarget target,
             ProjectBuildJob job )
         {
+            string old = Directory.GetCurrentDirectory();
+            Directory.SetCurrentDirectory(projectRoot);
             ProgramRunner.Run( job.Arguments );
+            Directory.SetCurrentDirectory(old);
         }
 
         #endregion
