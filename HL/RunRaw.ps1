848fc86f557d7964a9c7889f961271a9df28a980
Tim Akermann <ti.akermann@gmail.com>
2021-05-12 17:17:26 +0200

    Updated Kernel Code.

--- HL/RunRaw.ps1
+++ HL/RunRaw.ps1
@@ -13,10 +13,6 @@ if ($decision -eq 0) {
     #Delete Old Disk if any
     echo "Removing Old Disks"
     Remove-Item 'D:\Users\Tim\Documents\viscpu\src\VisCPU.Console\bin\Debug\net5.0\configs\cpu\peripherals\FileDrive' -Recurse
-    
-    #Initialize Disks
-    echo "Creating Partitions..."
-    vis project make ./Kernel ReleaseRun >$null 2>&1
 
 }
 
